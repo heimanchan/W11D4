0d7b68f885e8cce8ad2f4f092cc2858ab9ea0ac7
Heiman Chan <heimanchan0@gmail.com>
2019-03-21 09:37:59 -0700

    First commit

---
+++ linked_list_project/lib/linked_list.js
@@ -0,0 +1,121 @@
+// ============================================================================
+// Implementation Exercise: Singly Linked List
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Singly Linked List and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Linked List reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+// TODO: Implement a Linked List Node class here
+class Node {
+    constructor(val) {
+      this.value = val;
+      this.next = null;
+    }
+}
+
+// TODO: Implement a Singly Linked List class here
+class LinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+
+    // TODO: Implement the addToTail method here
+    addToTail(val) {
+      let newNode = new Node(val)
+
+      if (this.head === null) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        let temp = this.tail;
+        temp.next = newNode;
+        this.tail = newNode;
+      }
+
+      this.length++;
+      return this
+    }
+
+    // TODO: Implement the removeTail method here
+    removeTail() {
+      let temp = this.tail;
+
+      if (this.length === 0) {
+        return undefined
+      } else if (this.length === 1) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        let currentNode = this.head;
+        for (let i = 0; i < this.length - 1; i++) {
+          if (currentNode.next === this.tail) {
+            this.tail = currentNode;
+            this.tail.next = null;
+          }
+          currentNode = currentNode.next;
+        }
+      }
+      this.length--;
+      return temp;
+    }
+
+    // TODO: Implement the addToHead method here
+    addToHead(val) {
+      
+    }
+
+    // TODO: Implement the removeHead method here
+    removeHead() {
+
+    }
+
+    // TODO: Implement the contains method here
+    contains(target) {
+
+    }
+
+    // TODO: Implement the get method here
+    get(index) {
+
+    }
+
+    // TODO: Implement the set method here
+    set(index, val) {
+
+    }
+
+    // TODO: Implement the insert method here
+    insert(index, val) {
+
+    }
+
+    // TODO: Implement the remove method here
+    remove(index) {
+
+    }
+
+    // TODO: Implement the size method here
+    size() {
+      return this.length;
+    }
+}
+
+exports.Node = Node;
+exports.LinkedList = LinkedList;
