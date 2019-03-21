eefa33bcd4898bd7c5be09424f33f3265ccd1dce
Heiman Chan <heimanchan0@gmail.com>
2019-03-21 10:16:14 -0700

    Finish linked list practice

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -78,37 +78,113 @@ class LinkedList {
 
     // TODO: Implement the addToHead method here
     addToHead(val) {
-      
+      let newNode = new Node(val);
+
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        let temp = this.head;
+        this.head = newNode;
+        newNode.next = temp;
+      }
+
+      this.length++;
+      return this;
     }
 
     // TODO: Implement the removeHead method here
     removeHead() {
+      if (!this.head) return undefined;
+      let temp = this.head;
 
+      if (this.length === 1) {
+        this.head = this.tail = null;
+      } else {
+        this.head = temp.next;
+      }
+
+      this.length--;
+      return temp;
     }
 
     // TODO: Implement the contains method here
     contains(target) {
-
+      let currentNode = this.head
+      for (let i = 0; i < this.length; i++) {
+        if (currentNode.value === target) {
+          return true;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+      return false;
     }
 
     // TODO: Implement the get method here
     get(index) {
+      if (index > this.length) {
+        return null;
+      }
 
+      let currentNode = this.head;
+      for (let i = 0; i < index; i++) {
+        currentNode = currentNode.next;
+      }
+
+      return currentNode;
     }
 
     // TODO: Implement the set method here
     set(index, val) {
+      if (index > this.length) return false;
 
+      let currentNode = this.head;
+      for (let i = 0; i < index; i++) {
+        currentNode = currentNode.next;
+      }
+
+      if (currentNode) {
+        currentNode.value = val;
+        return true;
+      } 
+      return false;
     }
 
     // TODO: Implement the insert method here
     insert(index, val) {
+      if (index >= this.length) return false;
 
+      let newNode = new Node(val);
+      let currentNode = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+
+      let temp = currentNode.next;
+      currentNode.next = newNode;
+      newNode.next = temp;
+      this.length++;
+
+      return true;
     }
 
     // TODO: Implement the remove method here
     remove(index) {
+      if (index >= this.length) return undefined;
 
+      let currentNode = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+
+      let removeNode = currentNode.next;
+      let temp = currentNode.next;
+      temp = temp.next;
+      currentNode.next = temp;
+      this.length--;
+
+      return removeNode;
     }
 
     // TODO: Implement the size method here
